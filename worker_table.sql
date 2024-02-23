ce4338e2aab5c18f018e2607d7768d726e483448
Alex <alexgofresh@gmail.com>
2024-02-23 22:57:53 +0100

    table created ,vacation_days and email unique added

---
+++ worker_table.sql
@@ -0,0 +1,17 @@
+CREATE TABLE workers(
+ id SERIAL PRIMARY KEY,
+ full_name VARCHAR(80) NOT NULL CHECK (full_name != ''),
+ birthday  DATE NOT NULL CHECK (birthday > '1996-01-01' AND birthday <= current_date ),
+ salary NUMERIC(10,2) NOT NULL DEFAULT 0.00,
+ created_at TIMESTAMP DEFAULT current_timestamp,
+ updated_at TIMESTAMP DEFAULT current_timestamp
+);
+
+--add  vacation_days
+ALTER TABLE workers 
+ADD COLUMN vacation_days INT DEFAULT 0 CHECK (vacation_days >= 0);
+
+-- add email
+ALTER TABLE workers
+ADD COLUMN email VARCHAR(80) UNIQUE;
+
