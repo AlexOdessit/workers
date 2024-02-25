81a30aafa086a2293d3c26588a00c7ce8e405abb
Alex <alexgofresh@gmail.com>
2024-02-26 00:15:41 +0100

    Svetlana added

--- insert.sql
+++ insert.sql
@@ -5,3 +5,7 @@ RENAME TO employees;
 -- adding Mikita
 INSERT INTO employees (full_name, birthday, email, salary)
 VALUES ('Микита', '1990-02-04', 'mikita@gmail.com', 600.00);
+
+-- adding Svetlana
+INSERT INTO employees (full_name, birthday, email, salary)
+VALUES ('Cвiтлана', '1995-04-08', 'sveta@gmail.com', 1200.00);
