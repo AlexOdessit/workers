5ca68689cc055aecab7874a138a09017311951ea
Alex <alexgofresh@gmail.com>
2024-02-26 15:43:46 +0100

    --increasing values where salary < 400

--- update.sql
+++ update.sql
@@ -7,3 +7,8 @@ WHERE full_name = 'Павло';
 UPDATE employees 
 SET birthday = '1987-07-14'
 WHERE id = 4;
+
+--increasing values where salary < 400
+UPDATE employees 
+SET salary = 700
+WHERE id IN(5,6);
